71e273b783e04211603aef8fae0275e16b9fe356
guutara <h2r3k6nisi@gmail.com>
2022-04-17 15:19:46 +0900

    "/"pathの継続日数が表示される際の処理を書いた

--- src/components/molecules/ContinuationDailyReportCount.jsx
+++ src/components/molecules/ContinuationDailyReportCount.jsx
@@ -1,23 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { LoadingAnimation } from "../atoms";
+
+import { GetTodayYYYYMMDD, GetYesterdayYYYYMMDD } from "../../utils/function";
+
+import { db } from "../../firebase";
+import { getDoc, doc } from "firebase/firestore";
 
 const ContinuationDailyReportCount = () => {
+	const { hyphenYYYYMMDD: todayHyphenDate } = GetTodayYYYYMMDD();
+	const { hyphenYYYYMMDD: yesterdayHyphenDate } = GetYesterdayYYYYMMDD();
+
+	const [loading, setLoading] = useState(true);
+	const [continuationDaysDocIsValid, setContinuationDaysDocIsValid] =
+		useState(false);
+
+	const [slashFirstDay, setSlashFirstDay] = useState("");
+	const [slashEndDay, setSlashEndDay] = useState("");
+	const [continuationCount, setContinuationCount] = useState();
+
+	useEffect(() => {
+		setLoading(true);
+		const judgeContinuationDaysDocIsValid = async () => {
+			const yesterdayDocSnap = await getDoc(
+				doc(db, "dailyReport", yesterdayHyphenDate)
+			);
+			const todayDocSnap = await getDoc(doc(db, "dailyReport", todayHyphenDate));
+
+			if (yesterdayDocSnap.exists() || todayDocSnap.exists()) {
+				setContinuationDaysDocIsValid(true);
+				const continuationDaysSnap = await getDoc(
+					doc(db, "dailyReportContinuationDays", "continuationDays")
+				);
+				console.log("continuationDaysSnap:", continuationDaysSnap.data());
+				setSlashFirstDay(continuationDaysSnap.data().slashFirstDay);
+				setSlashEndDay(continuationDaysSnap.data().slashEndDay);
+				setContinuationCount(continuationDaysSnap.data().count);
+				setLoading(false);
+			} else {
+				setContinuationDaysDocIsValid(false);
+				console.log("日報が連続して登録されていません");
+				setContinuationCount(0);
+				setLoading(false);
+			}
+		};
+		judgeContinuationDaysDocIsValid();
+	}, []);
 	return (
-		<div className="flex flex-row flex-wrap justify-around items-center gap-y-3">
-			<div className="h-[60vh] flex flex-col justify-center items-center min-w-[310px] gap-y-2">
-				<div className="text-lg text-4xl  font-bold text-indigo-400/80">
-					継続日数
+		<div>
+			{loading ? (
+				<div className="h-[55vh] flex items-center">
+					<LoadingAnimation />
 				</div>
-				<div className="flex flex-row justify-center items-end">
-					<div className="text-7xl md:text-9xl font-black text-indigo-400/80">
-						18
+			) : (
+				<div className="flex flex-row flex-wrap justify-around items-center gap-y-3">
+					<div className="h-[60vh] flex flex-col justify-center items-center min-w-[310px] gap-y-2">
+						<div className="text-lg text-4xl  font-bold text-indigo-400/80">
+							継続日数
+						</div>
+						<div className="flex flex-row justify-center items-end">
+							<div className="text-7xl md:text-9xl font-black text-indigo-400/80">
+								{continuationCount}
+							</div>
+							<div className="text-5xl md:text-7xl font-black text-indigo-400/80">
+								日
+							</div>
+						</div>
+						{continuationDaysDocIsValid && (
+							<div>{`${slashFirstDay} ~ ${slashEndDay}`}</div>
+						)}
 					</div>
-					<div className="text-5xl md:text-7xl font-black text-indigo-400/80">
-						日
-					</div>
+					<div>手書きでマグロの絵を書く</div>
 				</div>
-				<div>2022/08/09 ~ 2022/08/09</div>
-			</div>
-			<div>手書きでマグロの絵を書く</div>
+			)}
 		</div>
 	);
 };
